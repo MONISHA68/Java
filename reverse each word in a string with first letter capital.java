7da2a2fbe5fda9e9fee1e40a2b534ae25e6d5ae6
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-03-23 22:53:02 +0530

    Update reverse each word in a string with first letter capital.java

--- reverse each word in a string with first letter capital.java
+++ reverse each word in a string with first letter capital.java
@@ -28,3 +28,37 @@ class GFG
 	    }     
 	}
 }
+
+
+//////////////////////////////////////////same problem with another method /////////////////////////
+import java.util.*;
+import java.lang.*;
+class GFG 
+{
+	public static void main (String[] args)
+	{
+	    Scanner sc=new Scanner(System.in);
+	    String s=sc.nextLine();
+	    int count=0;
+	    String str[]=s.split("\\s");
+	   //System.out.println(str.length);
+	    for(int i=0;i<str.length;i++)
+	    {
+	        count=0;
+	     for(int j=str[i].length()-1;j>=0;j--)
+	     {
+	         count++;
+	         if(count==str[i].length())
+	         {
+	             String b=String.valueOf(str[i].charAt(j));
+	             System.out.print(b.toUpperCase());
+	         }
+	         else
+	         {
+	             System.out.print(str[i].charAt(j));
+	         }
+	     }
+	     System.out.print(" ");
+	    }     
+	}
+}
