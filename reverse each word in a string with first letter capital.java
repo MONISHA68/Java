6dc1e07a8562cee54f1196edec31abad8b04c989
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-03-23 22:39:23 +0530

    Create reverse each word in a string with first letter capital.java

---
+++ reverse each word in a string with first letter capital.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+class GFG 
+{
+	public static void main (String[] args)
+	{
+	    Scanner sc=new Scanner(System.in);
+	    String s=sc.nextLine();
+	    int count=0;
+	    String str[]=s.split("\\s");
+	   //System.out.println(str.length);
+	    for(int i=0;i<str.length;i++)
+	    {
+	        count=0;
+	     for(int j=str[i].length()-1;j>=0;j--)
+	     {
+	         count++;
+	         if(count==str[i].length())
+	         {
+	             System.out.print(Character.toUpperCase(str[i].charAt(j)));
+	         }
+	         else
+	         {
+	             System.out.print(str[i].charAt(j));
+	         }
+	     }
+	     System.out.print(" ");
+	    }     
+	}
+}
